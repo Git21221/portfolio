559fc6b4723dcd97043c255d24fc322bcaff4344
Saikat Das <saikatdas40g@gmail.com>
2024-03-21 19:05:01 +0530

    added other

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-let images = [
-  '1.png',
-  '2.png',
-  '3.png',
-  '4.png',
-];
+let images = ["1.png", "2.png", "3.png", "4.png"];
 let githubLink = [
-  'https://github.com/Git21221/PlayBeatz',
-  'https://github.com/Git21221/JS-beginner-projects',
-  'https://github.com/Git21221/IBMSkillsBuild',
-  'https://github.com/Git21221/Blog-Full-Stack',
-]
+  "https://github.com/Git21221/PlayBeatz",
+  "https://github.com/Git21221/JS-beginner-projects",
+  "https://github.com/Git21221/IBMSkillsBuild",
+  "https://github.com/Git21221/Blog-Full-Stack",
+];
 
-const imageContainer = document.querySelector('.imageContainer');
+const imageContainer = document.querySelector(".imageContainer");
 
-const img = document.createElement('img');
+const img = document.createElement("img");
 
 let temp = 0;
 img.src = images[temp];
 left.after(img);
 
-left.addEventListener('click', () => {
-  temp == 0 ? temp = images.length - 1 : temp--;
+left.addEventListener("click", () => {
+  temp == 0 ? (temp = images.length - 1) : temp--;
   img.src = images[temp];
   left.after(img);
-})
+});
 
-right.addEventListener('click', () => {
-  temp == images.length - 1 ? temp = 0 : temp++;
+right.addEventListener("click", () => {
+  temp == images.length - 1 ? (temp = 0) : temp++;
   img.src = images[temp];
   left.after(img);
-})
-view.addEventListener('click', () => {
+});
+view.addEventListener("click", () => {
   window.open(githubLink[temp]);
-})
+});
 
-
-
-body.addEventListener('mousemove', () => {
+body.addEventListener("mousemove", () => {
   const rect = body.getBoundingClientRect();
   const x = ((event.clientX - rect.left) / body.offsetWidth) * 100;
   const y = ((event.clientY - rect.top) / body.offsetHeight) * 100;
 
   gradient.style.left = x + "%";
-  gradient.style.boxShadow = '10px 10px 300px 80px rgb(0, 112, 240)';
+  gradient.style.boxShadow = "10px 10px 300px 80px rgb(0, 112, 240)";
   gradient.style.top = y + "%";
-})
+});
 
-works.addEventListener('mousemove', () => {
+works.addEventListener("mousemove", () => {
   const rect = works.getBoundingClientRect();
   const x = ((event.clientX - rect.left) / works.offsetWidth) * 100;
   const y = ((event.clientY - rect.top) / works.offsetHeight) * 100;
 
   gradient_green.style.left = x + "%";
-  gradient_green.style.boxShadow = '10px 10px 300px 80px rgba(94, 255, 0, 0.411)';
+  gradient_green.style.boxShadow =
+    "10px 10px 300px 80px rgba(94, 255, 0, 0.411)";
   gradient_green.style.top = y + 100 + "%";
-})
+});
+
+const sendMail = () => {
+  const sub = `${subject.value} from (${username.value})`;
+  window.open(
+    `mailto:saikatdas40g@gmail.com?body=${bodyofmail.value}&subject=${sub}`
+  );
+};
